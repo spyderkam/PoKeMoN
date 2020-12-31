6ac83640cd4ba59a5c0bed29df663227bba27fa2
Kam <58607149+spyderkam@users.noreply.github.com>
2020-12-31 14:11:10 -0800

    Create database.rb

---
+++ database.rb
@@ -0,0 +1,14 @@
+squad = [] #self
+
+$gym_leader = ["Fearow", "Flaaffy", "Staryu"]
+
+$pokedex = {
+  "Fearow" => 2,
+  "Flaaffy" => 3,
+  "Krabby" => 2,
+  "Magikarp" => 0,
+  "Onix" => 3,
+  "Pidgey" => 1,
+  "Rattata" => 1,
+  "Staryu" => 2
+}
